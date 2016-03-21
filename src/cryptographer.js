a2c13ee9a2a1c7fa55bf36363e28f81d0ab5fda7
Peter Beverloo <peter@chromium.org>
2016-03-21 17:23:32 +0000

    Pad value should be written in network byte order
    Patch by @marco-c in the following PR (thanks!):
      https://github.com/beverloo/peter.sh/pull/17

--- src/cryptographer.js
+++ src/cryptographer.js
@@ -104,7 +104,7 @@ class WebPushCryptographer {
 
     }).then(([contentEncryptionKey, nonce]) => {
       const record = new Uint8Array(2 + paddingBytes + plaintext.byteLength);
-      record.set([ paddingBytes & 0xFF, paddingBytes >> 8 ]);
+      record.set([ paddingBytes >> 8, paddingBytes & 0xFF ]);
       record.fill(0, 2 /* sizeof(uint16_t) */, 2 + paddingBytes);
       record.set(new Uint8Array(plaintext), 2 + paddingBytes);
 
